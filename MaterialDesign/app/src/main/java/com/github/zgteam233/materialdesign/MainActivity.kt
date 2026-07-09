2cee4843e146db8bfba9881fdec122a7d80ae139
ZGTeam233 <wsnzg6@outlook.com>
2026-07-09 08:55:54 +0800

    -

--- MaterialDesign/app/src/main/java/com/github/zgteam233/materialdesign/MainActivity.kt
+++ MaterialDesign/app/src/main/java/com/github/zgteam233/materialdesign/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.github.zgteam233.materialdesign
 
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.github.zgteam233.materialdesign.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                binding.drawerLayout.openDrawer(GravityCompat.START)
+            }
+            R.id.backup -> "You clicked Backup".showToast()
+            R.id.delete -> "You clicked Delete".showToast()
+            R.id.settings -> "You clicked Settings".showToast()
+        }
+        return true
+    }
+
 }
